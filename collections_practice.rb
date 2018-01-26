eef59815c1250943ab79b6cb52171fe96c9f06ae
Mars Wong <marswong618@gmail.com>
2018-01-26 05:33:31 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    else
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(arr)
+  arr.sort do |a, b|
+    if a.downcase < b.downcase
+      -1
+    else
+      1
+    end
+  end
+end
+
+def swap_elements(arr)
+  arr[1] = "#{arr[1]}:)#{arr[2]}"
+  arr[2] = arr[1].split(":)")[0]
+  arr[1] = arr[1].split(":)")[1]
+  arr
+end
+
+def swap_elements_from_to(arr, from, to)
+  arr[from] = "#{arr[from]}:)#{arr[to]}"
+  arr[to] = arr[from].split(":)")[0]
+  arr[from] = arr[from].split(":)")[1]
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.map { |x| (x[2] = "$") && x }
+end
+
+def find_a(arr)
+  arr.select { |x| x.start_with?("a") }
+end
+
+def sum_array(arr)
+  arr.inject(:+)
+end
+
+def add_s(arr)
+  arr.collect.each_with_index { |x, i| i == 1 ? x : "#{x}s" }
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -28,6 +28,10 @@ describe 'collections practice' do
     it 'swap the second and third elements of an array' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
+    it "swap elements at from and to indices of an array" do
+      expect(swap_elements_from_to(["a", "b", "c"], 0, 2)).to eq(["c", "b", "a"])
+      expect(swap_elements_from_to(["a", "b", "c"], 2, 1)).to eq(["a", "c", "b"])
+    end
   end
 
   # Question 5
